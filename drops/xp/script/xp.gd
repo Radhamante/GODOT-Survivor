dcb91f44e8e65a82af525efe5c1056d04e30eeb3
Radhamante <raphael.delassus@proton.me>
2025-04-24 12:45:20 +0200

    feat : stack xp

--- drops/xp/script/xp.gd
+++ drops/xp/script/xp.gd
@@ -2,9 +2,37 @@ extends Magnetable
 class_name XPMagnetable
 
 var value: float
+var merged := false
+
+func _modulate_on_value() -> void:
+	var h = 190.0 / 360.0
+	var s = clamp(0.2 + (value * 0.1), 0.0, 1.0)
+	var v = 1.0 
+	modulate = Color.from_hsv(h, s, v)
+	
+	scale = Vector2.ONE * (1.0 + clamp((value * 0.05), .0, .5))
+
+func _ready() -> void:
+	_modulate_on_value()
 	
 func _physics_process(delta: float) -> void:
 	if not entity:
 		return
 	var direction = (entity.global_position - global_position).normalized()
 	global_position += direction * speed * delta
+
+
+func _on_area_entered(magnetable: Magnetable) -> void:
+	if merged or not is_instance_valid(magnetable):
+		return
+
+	if magnetable is XPMagnetable:
+		var other: XPMagnetable = magnetable
+
+		# Évite le double merge
+		if self.get_instance_id() < other.get_instance_id():
+			# On garde celui avec l'ID le plus bas
+			value += other.value
+			other.merged = true
+			other.queue_free()
+			_modulate_on_value()
